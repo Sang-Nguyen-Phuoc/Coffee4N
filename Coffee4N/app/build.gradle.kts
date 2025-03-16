23844a7dce3dff6acd155c4786b8635608c7abd5
Sang-Nguyen-Phuoc <nguyenphuocsang04012004@gmail.com>
2025-03-16 20:27:58 +0700

    #32 - Sang-Nguyen-Phuoc - set up firebase

--- Coffee4N/app/build.gradle.kts
+++ Coffee4N/app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android") version "2.0.0" // Thay đổi từ 1.9.22 thành 2.0.0
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" // Cập nhật để khớp với Kotlin
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -53,6 +54,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
     implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation(libs.firebase.database)
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
@@ -61,4 +63,6 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation(platform("com.google.firebase:firebase-bom:33.10.0"))
+    implementation("com.google.firebase:firebase-analytics")
 }
